94a3712cbaf5a756e0f57a2ba13bee14fa7aba5a
Marko Sibakov <marko.sibakov@reaktor.fi>
2009-01-29 20:42:52 +0000

    added DataView that contains list of account ids.

--- src/main/java/org/laughingpanda/AccountsPanel.java
+++ src/main/java/org/laughingpanda/AccountsPanel.java
@@ -1,6 +1,12 @@
 package org.laughingpanda;
 
+import java.util.Arrays;
+
+import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.panel.Panel;
+import org.apache.wicket.markup.repeater.Item;
+import org.apache.wicket.markup.repeater.data.DataView;
+import org.apache.wicket.markup.repeater.data.ListDataProvider;
 
 /**
  * @author Markus Hjort / Reaktor Innovations Oy
@@ -8,5 +14,12 @@ import org.apache.wicket.markup.html.panel.Panel;
 public class AccountsPanel extends Panel {
 	public AccountsPanel(String id) {
 		super(id);
+		add(new DataView<String>("accounts", new ListDataProvider<String>(Arrays.asList("9500-12345","9500-56789"))) {
+
+			@Override
+			protected void populateItem(Item<String> item) {
+				item.add(new Label("accountId", item.getDefaultModelObjectAsString()));
+			}
+		});
 	}
 }
--- src/test/java/org/laughingpanda/AccountsPanelSpec.java
+++ src/test/java/org/laughingpanda/AccountsPanelSpec.java
@@ -1,8 +1,13 @@
 package org.laughingpanda;
 
+import java.util.List;
+
 import jdave.junit4.JDaveRunner;
 import jdave.wicket.ComponentSpecification;
 
+import org.apache.wicket.Component;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.repeater.data.DataView;
 import org.apache.wicket.model.IModel;
 import org.junit.runner.RunWith;
 
@@ -11,14 +16,28 @@ import org.junit.runner.RunWith;
  */
 @RunWith(JDaveRunner.class)
 public class AccountsPanelSpec extends ComponentSpecification<AccountsPanel, Void> {
-	public class MultipleTransactions {
+	public class PanelContainingMultipleTransactions {
 		public AccountsPanel create() {
 			return startComponent();
 		}
 		
-		public void rendersPanel() {}
+		public void containsDataView() {
+			specify(accountsView(), isNotNull());
+		}
+
+		
+		public void containsAccountIdLabels() {
+			List<Component> accounts = selectAll(Label.class, "accountId").from(accountsView());
+			specify(accounts.size(), does.equal(2));
+			specify(accounts.get(0).getDefaultModelObjectAsString(), does.equal("9500-12345"));
+			specify(accounts.get(0).getDefaultModelObjectAsString(), does.equal("9500-12345"));
+		}
 	}
 	
+	private DataView<String> accountsView() {
+		return selectFirst(DataView.class, "accounts").from(context);
+	}
+
 	@Override
 	protected AccountsPanel newComponent(String id, IModel<Void> model) {
 		return new AccountsPanel(id);
