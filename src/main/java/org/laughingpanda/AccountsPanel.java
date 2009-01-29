26ac86cdc7f1c10ec471d857b6796f95bb7d6c80
Timo Rantalaiho <timo.rantalaiho@reaktor.fi>
2009-01-29 13:39:01 +0000

    Removed strange JUnit and JDave stuff from production codez.

--- src/main/java/org/laughingpanda/AccountsPanel.java
+++ src/main/java/org/laughingpanda/AccountsPanel.java
@@ -1,14 +1,10 @@
 package org.laughingpanda;
 
-import jdave.junit4.JDaveRunner;
-
 import org.apache.wicket.markup.html.panel.Panel;
-import org.junit.runner.RunWith;
 
 /**
  * @author Markus Hjort / Reaktor Innovations Oy
  */
-@RunWith(JDaveRunner.class)
 public class AccountsPanel extends Panel {
 	public AccountsPanel(String id) {
 		super(id);
