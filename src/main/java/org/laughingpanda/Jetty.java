9cad49c6c575edc5672da77bb61d758abaa357e8
Marko Sibakov <marko.sibakov@reaktor.fi>
2009-01-29 12:41:37 +0000

    Improved jetty starter to support web driver jetty starter. Added web driver jetty starter.

--- src/main/java/org/laughingpanda/Jetty.java
+++ src/main/java/org/laughingpanda/Jetty.java
@@ -9,17 +9,54 @@ import org.mortbay.jetty.webapp.WebAppContext;
  * @author Marko Sibakov / Reaktor Innovations Oy
  */
 public class Jetty {
-    public static void main(String[] args) throws Exception {
-        Server server = new Server();
-        SelectChannelConnector connector = new  SelectChannelConnector();
-        connector.setPort(8080);
-        server.setConnectors(new Connector[] { connector });
+    private static final int PORT = 8080;
+    protected static final String CONTEXT_PATH = "/kansanpankki";
+    private static final String WAR = "src/main/webapp";
+	private Server server;
 
-        WebAppContext web = new WebAppContext();
-        web.setContextPath("/kansanpankki");
-        web.setWar("src/main/webapp");
-        server.addHandler(web);
-        server.start();
-        server.join();
+    public static void main(String[] args) throws Exception {
+        new Jetty().start();
+    }
+    
+    public void start() {
+        server = new Server();
+        server.setConnectors(getConnectors());
+        server.addHandler(getWebApplicationContext());
+        startServer();
+        joinServer();
+    }
+    
+    private void joinServer() {
+        new Thread(new Runnable() {
+            public void run() {
+                try {
+                    server.join();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }).start();
+    }
+
+    protected Connector[] getConnectors() {
+        SelectChannelConnector connector = new SelectChannelConnector();
+        connector.setPort(PORT);
+
+        return new Connector[] { connector };
+    }
+    
+    private void startServer() {
+        try {
+            server.start();
+        } catch (Exception e1) {
+            throw new RuntimeException(e1);
+        }
+    }
+    
+    protected WebAppContext getWebApplicationContext() {
+        WebAppContext app = new WebAppContext();
+        app.setContextPath(CONTEXT_PATH);
+        app.setWar(WAR);
+        return app;
     }
 }
---
+++ src/main/java/org/laughingpanda/WebDriverJettyStarter.java
@@ -0,0 +1,23 @@
+package org.laughingpanda;
+
+import org.mortbay.jetty.Connector;
+import org.mortbay.jetty.nio.SelectChannelConnector;
+
+
+/**
+ * @author Marko Sibakov / Reaktor Innovations Oy
+ */
+public class WebDriverJettyStarter extends Jetty {
+    private static final int PORT = 8082;
+    
+    public static void main(String... args) {
+        new WebDriverJettyStarter().start();
+    }
+    
+    @Override
+    protected Connector[] getConnectors() {
+        SelectChannelConnector connector = new SelectChannelConnector();
+        connector.setPort(PORT);
+        return new Connector[] { connector };
+    }
+}
