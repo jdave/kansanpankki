dcb44f4789f4b082474c85ffb48d3b51aa69fb98
Marko Sibakov <marko.sibakov@reaktor.fi>
2009-03-16 08:38:16 +0000

    added license text

--- src/test/java/org/laughingpanda/kansanpankki/accounts/AccountsPanelSpec.java
+++ src/test/java/org/laughingpanda/kansanpankki/accounts/AccountsPanelSpec.java
@@ -1,3 +1,19 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.laughingpanda.kansanpankki.accounts;
 
 import java.util.Arrays;
@@ -8,11 +24,9 @@ import jdave.wicket.ComponentSpecification;
 
 import org.apache.wicket.Component;
 import org.apache.wicket.ajax.markup.html.AjaxLink;
-import org.apache.wicket.ajax.markup.html.form.AjaxSubmitButton;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.form.Button;
 import org.apache.wicket.markup.html.form.Form;
-import org.apache.wicket.markup.html.form.TextField;
 import org.apache.wicket.markup.repeater.data.DataView;
 import org.apache.wicket.markup.repeater.data.ListDataProvider;
 import org.apache.wicket.model.IModel;
@@ -20,7 +34,6 @@ import org.apache.wicket.util.tester.FormTester;
 import org.jmock.Expectations;
 import org.junit.runner.RunWith;
 import org.laughingpanda.kansanpankki.account.AccountPage;
-import org.laughingpanda.kansanpankki.dao.AccountDao;
 import org.laughingpanda.kansanpankki.domain.Account;
 import org.laughingpanda.kansanpankki.domain.AccountRepository;
 
